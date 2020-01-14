ac7f1da73db6357611ab8e3417f630f6a831ec9e
namiwa <khairul.nasri895@gmail.com>
2020-01-14 14:48:05 +0800

    feat: adding basic associate component

---
+++ src/routes/associate/Associate.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Canvas from './Canvas/canvas';
+
+import './Associate.scss';
+
+const Associate: React.FC = () => {
+  return (
+    <div className="columns is-centered is-marginless is-paddingless">
+      <div className="column is-four-fifths">
+        <div className="is-centered">pLEASE SHOW SOMETHING</div>
+        <Canvas />
+      </div>
+    </div>
+  );
+};
+
+export default Associate;
