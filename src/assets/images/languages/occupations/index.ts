cd722396e169dac7878649af8e886067a6d78c98
Hanming Zhu <hanming@huawen.com>
2020-02-17 04:34:21 +0800

    feat: add svgr for occupations

---
+++ src/assets/images/languages/occupations/index.ts
@@ -0,0 +1,23 @@
+export { default as Astronaut } from './Astronaut';
+export { default as Baker } from './Baker';
+export { default as Barber } from './Barber';
+export { default as Cashier } from './Cashier';
+export { default as Chef } from './Chef';
+export { default as Doctor } from './Doctor';
+export { default as Farmer } from './Farmer';
+export { default as Fireman } from './Fireman';
+export { default as Librarian } from './Librarian';
+export { default as Nurse } from './Nurse';
+export { default as Painter } from './Painter';
+export { default as Pilot } from './Pilot';
+export { default as Policeman } from './Policeman';
+export { default as Scientist } from './Scientist';
+export { default as Student } from './Student';
+export { default as Teacher } from './Teacher';
+export { default as Vet } from './Vet';
+export { default as Waiter } from './Waiter';
+
+export interface SvgrProps {
+  height?: any;
+  width?: any;
+}
