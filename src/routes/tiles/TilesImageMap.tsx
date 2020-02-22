82238cc7f3602d23f22253664ee71974cd2ea04d
namiwa <khairul.nasri895@gmail.com>
2020-02-23 01:27:20 +0800

    feat(Tiles): Added image mapping component

---
+++ src/routes/tiles/TilesImageMap.tsx
@@ -0,0 +1,15 @@
+import BLACK_BLUE_BTM_LEFT from 'assets/images/tiles/black_blue_btm_left.svg';
+import BLACK_BLUE_BTM_RIGHT from 'assets/images/tiles/black_blue_btm_right.svg';
+import BLACK_BLUE_TOP_LEFT from 'assets/images/tiles/black_blue_top_left.svg';
+import BLACK_BLUE_TOP_RIGHT from 'assets/images/tiles/black_blue_top_right.svg';
+import BLUE_TILE from 'assets/images/tiles/blue_tile.svg';
+
+export const TilesImageMap = [
+  BLACK_BLUE_BTM_LEFT,
+  BLACK_BLUE_BTM_RIGHT,
+  BLACK_BLUE_TOP_LEFT,
+  BLACK_BLUE_TOP_RIGHT,
+  BLUE_TILE
+];
+
+export default TilesImageMap;
