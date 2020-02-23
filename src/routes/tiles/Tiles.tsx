3871598615b5ebb51b2b3f0fd0f899dfcaab4ccc
namiwa <khairul.nasri895@gmail.com>
2020-02-23 20:07:32 +0800

    feat(tiles): added basic styling to game

--- src/routes/tiles/Tiles.tsx
+++ src/routes/tiles/Tiles.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import TilesImage from './TilesImageMap';
 
 import './Tiles.scss';
 
+const Grey: React.FC = TilesImage.UnchoosenTile;
+const list = TilesImage.TilesImageMap();
+
 const Tiles: React.FC = () => {
   return (
-    <div className="tiles">
-      <p>This is the tiles page</p>
+    <div className="container notification is-warning">
+      <div className="tile is-ancestor is-mobile">
+        <div className="tile is-vertical">
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child notification is-primary">
+                <p className="title">Welcome to the tiles game</p>
+                <p className="subtitle">
+                  Click on the right tiles matching the pattern shown!
+                </p>
+              </article>
+              <article className="tile is-child notification is-warning">
+                <p className="title">...tiles</p>
+                <p className="subtitle">Bottom tile</p>
+                <Grey />
+                {list.map(val => {
+                  return (
+                    <figure className="image is-128x128">
+                      <img src={val} alt={val} />
+                    </figure>
+                  );
+                })}
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
