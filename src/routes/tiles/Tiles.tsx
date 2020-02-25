9ce36d1873e713af442a3686e7edc5a5d442a111
namiwa <khairul.nasri895@gmail.com>
2020-02-25 22:55:18 +0800

    feat(tiles): added basic structure and game logic

--- src/routes/tiles/Tiles.tsx
+++ src/routes/tiles/Tiles.tsx
@@ -1,43 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TilesImage from './TilesImageMap';
 
 import './Tiles.scss';
-
-const Grey: React.FC = TilesImage.UnchoosenTile;
-const list = TilesImage.TilesImageMap();
 
 // get static state (or rando) -> choose random 4 indexs -> create image place holders -> set to match on click -> show if match
 // requires console buttons to check answer / refresh game
 
 const Tiles: React.FC = () => {
+  const [gameState] = useState({
+    list: TilesImage.TilesImageMap()
+  });
+  const Grey: React.FC = TilesImage.UnchoosenTile;
+
+  const getFourRandomFromArray = (array: Array<string>) => {
+    const indexList = array.map((val, index) => index);
+    const randomArray = indexList.sort(() => 0.5 - Math.random());
+    return randomArray.slice(0, 4);
+  };
+
+  const [randomList, setRandom] = useState({
+    list: getFourRandomFromArray(gameState.list),
+    ansStack: [-1]
+  });
+
+  /* global HTMLButtonElement, MouseEvent */
+  const handleMouseClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    data: number
+  ) => {
+    e.preventDefault();
+    const copy = randomList.ansStack;
+    copy.push(data);
+    setRandom({
+      ...randomList,
+      ansStack: copy
+    });
+  };
+
+  const DisplayRandom: React.FC = () => {
+    return (
+      <div className="columns is-mobile">
+        <figure className="column image is-128by128 is-2 has-text-centered has-text-black">
+          <p> Match accordingly! </p>
+        </figure>
+        {randomList.list.map((val, ind) => {
+          const src = gameState.list[val];
+          return (
+            <figure className="column image is-128by128 is-2" key={val}>
+              <img src={src} alt={ind.toString()} />
+            </figure>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const PlayArea: React.FC = () => {
+    return <div>Play Area! </div>;
+  };
+
+  const ImHeader: React.FC = () => {
+    return (
+      <div className="columns is-mobile">
+        {gameState.list.map((val, ind) => {
+          return (
+            <figure className="column image is-128by128" key={val}>
+              <button onClick={e => handleMouseClick(e, ind)} type="button">
+                <img src={val} alt={ind.toString()} />
+              </button>
+            </figure>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <div className="container notification is-warning">
-      <div className="tile is-ancestor is-mobile">
-        <div className="tile is-vertical">
-          <div className="tile">
-            <div className="tile is-parent is-vertical">
-              <article className="tile is-child notification is-primary has-text-centered">
-                <p className="title">Welcome to the tiles game</p>
-                <p className="subtitle">
-                  Click on the right tiles matching the pattern shown!
-                </p>
-              </article>
-              <article className="tile is-child notification is-warning">
-                <p className="title">...tiles</p>
-                <p className="subtitle">Bottom tile</p>
-                <Grey />
-                {list.map(val => {
-                  return (
-                    <figure className="image is-128x128">
-                      <img src={val} alt={val} />
-                    </figure>
-                  );
-                })}
-              </article>
-            </div>
-          </div>
-        </div>
+      <div>
+        <article className="has-text-centered has-text-black">
+          <p>
+            Welcome to the tiles game, click on the right tiles matching the
+            pattern shown!
+          </p>
+        </article>
       </div>
+      <ImHeader />
+      <DisplayRandom />
+      <Grey />
+      <PlayArea />
     </div>
   );
 };
